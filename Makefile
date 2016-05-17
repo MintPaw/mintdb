0afa50bd929198cfcba4e24e5cb8e5a1d4ec0be2
MintPaw <jerusanders@gmail.com>
2016-05-17 16:45:38 -0600

    Stepped through instructions and disabled signal blocking for a bit

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 all: 
 	gcc -g -Wall main.c -o mintdb
 
+testEcho:
+	make all
+	./mintdb echo hi
+
 testLs:
 	make all
 	./mintdb ls -l
--- main.c
+++ main.c
@@ -3,22 +3,33 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/ptrace.h>
+#include <sys/wait.h>
 
 int main(int argc, char **argv)  {
-	sigset_t mask;
-	sigemptyset(&mask);
-	sigaddset(&mask, SIGCHLD);
-	sigprocmask(SIG_BLOCK, &mask, NULL);
+	/* sigset_t mask; */
+	/* sigemptyset(&mask); */
+	/* sigaddset(&mask, SIGCHLD); */
+	/* sigprocmask(SIG_BLOCK, &mask, NULL); */
+	/* sigprocmask(SIG_UNBLOCK, &mask, NULL); */
 
-	pid_t pid = fork();
+	pid_t childPid = fork();
 
-	if (pid != 0) {
-		// listen for sigchld
-		sigprocmask(SIG_UNBLOCK, &mask, NULL);
-		// attach
-	} else {
-		sigprocmask(SIG_UNBLOCK, &mask, NULL);
-		execvp(argv[1], &argv[1]);
+	if (childPid == 0) {
+		ptrace(PTRACE_TRACEME, 0, 0, 0);
+		if (execvp(argv[1], &argv[1]) < 0) perror("execvp");
 	}
+
+	// listen for sigchld
+	// attach
+	int childStatus;
+	int instruction = 0;
+	wait(&childStatus);
+	while (WIFSTOPPED(childStatus)) {
+		instruction++;
+		if (ptrace(PTRACE_SINGLESTEP, childPid, 0, 0) < 0) perror("ptrace");
+		wait(&childStatus);
+	}
+	printf("(%d) %d instruct\n", childPid, instruction);
+
 	return 0;
 }
