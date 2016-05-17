094c5c8101ac47d356f079b8b2c49538383751c7
MintPaw <jerusanders@gmail.com>
2016-05-17 14:42:14 -0600

    Exec-ed child and blocked signals

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/ptrace.h>
 
-int main() {
+int main(int argc, char **argv)  {
+	sigset_t mask;
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGCHLD);
+	sigprocmask(SIG_BLOCK, &mask, NULL);
 
+	pid_t pid = fork();
+
+	if (pid != 0) {
+		// listen for sigchld
+		sigprocmask(SIG_UNBLOCK, &mask, NULL);
+		// attach
+	} else {
+		sigprocmask(SIG_UNBLOCK, &mask, NULL);
+		execvp(argv[1], &argv[1]);
+	}
 	return 0;
 }
