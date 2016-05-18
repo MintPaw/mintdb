c55e13fbf4a109b7d1fdd10a33d2839f33977bda
MintPaw <jerusanders@gmail.com>
2016-05-17 18:26:33 -0600

    Split testProgSimple message into two messages

--- src/testProgSimple.asm
+++ src/testProgSimple.asm
@@ -1,14 +1,21 @@
 SECTION .data
-	msg:	db "Hello World",10	; the string to print, 10=cr
-	len:	equ $-msg		; "$" means "here"
-	; len is a value, not an address
+	msg1:	db "Hello",0xa
+	len1:	equ $-msg1
+	msg2: db "world!", 0xa
+	len2: equ $-msg2
 
 SECTION .text
 	global _start		; make label available to linker 
 
 _start:
-	mov	edx,len		; arg3, length of string to print
-	mov	ecx,msg		; arg2, pointer to string
+	mov	edx,len1		; arg3, length of string to print
+	mov	ecx,msg1		; arg2, pointer to string
+	mov	ebx,1		; arg1, where to write, screen
+	mov	eax,4		; write sysout command to int 80 hex
+	int	0x80		; interrupt 80 hex, call kernel
+
+	mov	edx,len2		; arg3, length of string to print
+	mov	ecx,msg2		; arg2, pointer to string
 	mov	ebx,1		; arg1, where to write, screen
 	mov	eax,4		; write sysout command to int 80 hex
 	int	0x80		; interrupt 80 hex, call kernel
