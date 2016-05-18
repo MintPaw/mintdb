488e71922936dfc155332cfded3f2388385286ed
MintPaw <jerusanders@gmail.com>
2016-05-17 19:42:23 -0600

    Added breakpoint setting on instructions

--- src/main.c
+++ src/main.c
@@ -7,12 +7,22 @@
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 
+#define TRAP_CODE 0xCC
+
+struct Breakpoint {
+	unsigned long long instrAddr;
+	long origInstr;
+};
+
+pid_t childPid;
+struct Breakpoint **breakpoints;
+int breakpointNum = 0;
+
 void setInstructionBreakpoint(unsigned long long instrNum);
-void sigchldHandler(int sig);
 
 int main(int argc, char **argv)  {
-
-	pid_t childPid = fork();
+	breakpoints = (struct Breakpoint **)malloc(sizeof(struct Breakpoint *) * 100);
+	childPid = fork();
 
 	if (childPid == 0) {
 		ptrace(PTRACE_TRACEME, 0, 0, 0);
@@ -22,16 +32,35 @@ int main(int argc, char **argv)  {
 	int childStatus;
 	wait(&childStatus);
 
+	setInstructionBreakpoint((unsigned long long)0x00000000004000da);
+
+	char firstTimeContinue = 1;
 	int instrNum = 0;
 	while (WIFSTOPPED(childStatus)) {
+		if (firstTimeContinue) {
+			firstTimeContinue = 0;
+			if (ptrace(PTRACE_CONT, childPid, 0, 0) < 0) perror("ptrace");
+			wait(&childStatus);
+			continue;
+		}
+
 		struct user_regs_struct regs;
 		ptrace(PTRACE_GETREGS, childPid, 0, &regs);
-		unsigned instr = ptrace(PTRACE_PEEKTEXT, childPid, regs.rip, 0);
+		long instr = ptrace(PTRACE_PEEKTEXT, childPid, regs.rip, 0);
 
-		printf("instrNum = %u.  RIP = 0x%08llx.  instr = 0x%08x\n", instrNum, regs.rip, instr);
-		/* printf("Stopped because %s\n", strsignal(WSTOPSIG(childStatus))); */
+		printf("Stopped. instrNum = %u.  RIP = 0x%llx.  instr = 0x%lx\n", instrNum, regs.rip, instr);
 
-		if (ptrace(PTRACE_SINGLESTEP, childPid, 0, 0) < 0) perror("ptrace");
+		int i;
+		for (i = 0; i < breakpointNum; i++) {
+			if (breakpoints[i]->instrAddr == regs.rip-1) {
+				printf("Stopped for breakpoint, fixing...\n");
+				ptrace(PTRACE_POKETEXT, childPid, breakpoints[i]->instrAddr, breakpoints[i]->origInstr);
+				regs.rip--;
+				ptrace(PTRACE_SETREGS, childPid, 0, &regs);
+			}
+		}
+
+		if (ptrace(PTRACE_CONT, childPid, 0, 0) < 0) perror("ptrace");
 		instrNum++;
 		wait(&childStatus);
 	}
@@ -39,9 +68,15 @@ int main(int argc, char **argv)  {
 	return 0;
 }
 
-void setInstructionBreakpoint(unsigned long long instrNum) {
+void setInstructionBreakpoint(unsigned long long instrAddr) {
+	long instr = ptrace(PTRACE_PEEKTEXT, childPid, (void*)instrAddr, 0);
+	printf("Adding breakpoint at instruction 0x%08llx\n", instrAddr);
 
-}
+	struct Breakpoint *b = (struct Breakpoint *)malloc(sizeof(struct Breakpoint));
+	b->instrAddr = instrAddr;
+	b->origInstr = instr;
+	breakpoints[breakpointNum++] = b;
 
-void sigchldHandler(int sig) {
+	long trapInstr = (instr & 0xFFFFFFFFFFFFFF00) | 0xCC;
+	ptrace(PTRACE_POKETEXT, childPid, (void*)instrAddr, trapInstr);
 }
